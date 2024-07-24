5d28841e02c2a54e80d423f87fd7d0eeaee5a097
yanwenqing <2732399562@qq.com>
2024-07-25 05:33:09 +0800

    111

--- notxv6/barrier.c
+++ notxv6/barrier.c
@@ -30,7 +30,21 @@ barrier()
   // Block until all threads have called barrier() and
   // then increment bstate.round.
   //
-  
+  pthread_mutex_lock(&bstate.barrier_mutex); 
+
+  bstate.nthread++;
+
+  if (bstate.nthread < nthread) {
+      pthread_cond_wait(&bstate.barrier_cond, &bstate.barrier_mutex);
+  }
+
+  if (bstate.nthread == nthread) {
+      bstate.round++;
+      bstate.nthread = 0;
+      pthread_cond_broadcast(&bstate.barrier_cond);
+  }
+
+  pthread_mutex_unlock(&bstate.barrier_mutex); 
 }
 
 static void *
@@ -76,3 +90,4 @@ main(int argc, char *argv[])
   }
   printf("OK; passed\n");
 }
+
--- notxv6/ph.c
+++ notxv6/ph.c
@@ -17,6 +17,7 @@ struct entry *table[NBUCKET];
 int keys[NKEYS];
 int nthread = 1;
 
+pthread_mutex_t lock[NBUCKET]; 
 
 double
 now()
@@ -51,8 +52,10 @@ void put(int key, int value)
     // update the existing key.
     e->value = value;
   } else {
+  pthread_mutex_lock(&lock[i]);
     // the new is new.
     insert(key, value, &table[i], table[i]);
+    pthread_mutex_unlock(&lock[i]);
   }
 
 }
@@ -117,6 +120,9 @@ main(int argc, char *argv[])
   for (int i = 0; i < NKEYS; i++) {
     keys[i] = random();
   }
+  
+  for (int i = 0; i < NBUCKET; ++i)
+    pthread_mutex_init(&lock[i], NULL);
 
   //
   // first the puts
